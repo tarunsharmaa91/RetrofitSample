848cafc9781eb5204f8b390d9f443401264f9b49
Tarun Sharma <Tarun.Sharma@digitaleconomysolutions.com>
2020-02-05 09:32:05 +0400

    All About GET requests in Retrofit

--- app/src/main/java/store/emaratech/ae/myapplication/JsonPlaceHolder.java
+++ app/src/main/java/store/emaratech/ae/myapplication/JsonPlaceHolder.java
@@ -1,9 +1,14 @@
 package store.emaratech.ae.myapplication;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 /**
  * Created By Tarun
@@ -12,4 +17,32 @@ public interface JsonPlaceHolder {
 
     @GET("posts")
     Call<List<Posts>> getPost();
+
+    @GET("posts/{id}/comments")
+    Call<List<Comments>> getComments(@Path("id") int postId);
+
+    //Query will add "?postId=1" to url
+    @GET("comments")
+    Call<List<Comments>> getCommentsQuery(@Query("id") int postId);
+
+    //Using below technique url will be like
+    // comments?id=1&_sort=id&_order=desc
+    @GET("comments")
+    Call<List<Comments>> getCommentsQueryWithMultipleParameters(
+            //@Query("id") Integer postId,
+            @Query("_sort") String sortBy,
+            @Query("_order") String orderBy,
+            @Query("id") Integer... posts
+    );
+
+    //Use of query map
+    @GET("comments")
+    Call<List<Comments>> getCommentsQueryWithMultipleParametersWithQueryMap(
+            @QueryMap Map<String, String> parameters
+    );
+
+    // Here we can pass simple Url like or we can use base url alongside and that will replace base url
+    // post/3/comments
+    @GET("comments")
+    Call<List<Comments>> getCommentsUsingURL(@Url String url);
 }
--- app/src/main/java/store/emaratech/ae/myapplication/MainActivity.java
+++ app/src/main/java/store/emaratech/ae/myapplication/MainActivity.java
@@ -8,15 +8,20 @@ import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
+
+    private JsonPlaceHolder jsonPlaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,149 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        JsonPlaceHolder jsonPlaceHolder = retrofit.create(JsonPlaceHolder.class);
+        jsonPlaceHolder = retrofit.create(JsonPlaceHolder.class);
+        //Normal Use of Get method
+        //getPosts();
+
+        //Get method with additional parameter
+        //getComments();
+
+        //Use of Single Query
+        //getCommentsWithQuery();
+
+        //Use of Multiple Query
+        //getCommentsWithMultipleQuery();
+
+        //Above thing we can achieve using QueryMap as well
+        getCommentsWithMultipleQueryUsingQueryMap();
+    }
+
+    private void getCommentsWithMultipleQueryUsingQueryMap() {
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("_sort","id");
+        hashMap.put("_order","desc");
+        hashMap.put("id","1");
+
+        Call<List<Comments>> call = jsonPlaceHolder.getCommentsQueryWithMultipleParametersWithQueryMap(hashMap);
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(@NotNull Call<List<Comments>> call, @NotNull Response<List<Comments>> response) {
+
+                if(response.isSuccessful())
+                {
+                    List<Comments> comments = response.body();
+                    assert comments != null;
+                    for(Comments comment : comments)
+                    {
+                        Log.e("Response : ", String.valueOf(comment.getId()));
+                    }
+                    Toast.makeText(MainActivity.this, "Success" , Toast.LENGTH_LONG).show();
+                }else
+                {
+                    Toast.makeText(MainActivity.this, response.code() , Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<List<Comments>> call, @NotNull Throwable t) {
+                Toast.makeText(MainActivity.this, "Fail" , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getCommentsWithMultipleQuery() {
+        // We can also pass null in place of id or desc
+
+        //or
+
+        //We can pass array as well
+        // new Integer[]{1,2,3}
+        //Call<List<Comments>> call = jsonPlaceHolder.getCommentsQueryWithMultipleParameters(4, "id", "desc",new Integer[]{1,2,3});
+
+        Call<List<Comments>> call = jsonPlaceHolder.getCommentsQueryWithMultipleParameters( "id", "desc",new Integer[]{1,2,3});
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(@NotNull Call<List<Comments>> call, @NotNull Response<List<Comments>> response) {
+
+                if(response.isSuccessful())
+                {
+                    List<Comments> comments = response.body();
+                    assert comments != null;
+                    for(Comments comment : comments)
+                    {
+                        Log.e("Response : ", String.valueOf(comment.getId()));
+                    }
+                    Toast.makeText(MainActivity.this, "Success" , Toast.LENGTH_LONG).show();
+                }else
+                {
+                    Toast.makeText(MainActivity.this, response.code() , Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<List<Comments>> call, @NotNull Throwable t) {
+                Toast.makeText(MainActivity.this, "Fail" , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getCommentsWithQuery() {
+        Call<List<Comments>> call = jsonPlaceHolder.getCommentsQuery(3);
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(@NotNull Call<List<Comments>> call, @NotNull Response<List<Comments>> response) {
+
+                if(response.isSuccessful())
+                {
+                    List<Comments> comments = response.body();
+                    assert comments != null;
+                    for(Comments comment : comments)
+                    {
+                        Log.e("Response : ", String.valueOf(comment.getPostId()));
+                    }
+                    Toast.makeText(MainActivity.this, "Success" , Toast.LENGTH_LONG).show();
+                }else
+                {
+                    Toast.makeText(MainActivity.this, response.code() , Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<List<Comments>> call, @NotNull Throwable t) {
+                Toast.makeText(MainActivity.this, "Fail" , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getComments() {
+        Call<List<Comments>> call = jsonPlaceHolder.getComments(3);
+        call.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(@NotNull Call<List<Comments>> call, @NotNull Response<List<Comments>> response) {
+
+                if(response.isSuccessful())
+                {
+                    List<Comments> comments = response.body();
+                    assert comments != null;
+                    for(Comments comment : comments)
+                    {
+                        Log.e("Response : ", String.valueOf(comment.getPostId()));
+                    }
+                    Toast.makeText(MainActivity.this, "Success" , Toast.LENGTH_LONG).show();
+                }else
+                {
+                    Toast.makeText(MainActivity.this, response.code() , Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call<List<Comments>> call, @NotNull Throwable t) {
+                Toast.makeText(MainActivity.this, "Fail" , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void getPosts() {
         Call<List<Posts>> call = jsonPlaceHolder.getPost();
         call.enqueue(new Callback<List<Posts>>() {
             @Override
